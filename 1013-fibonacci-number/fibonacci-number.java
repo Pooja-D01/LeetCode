e66fc04987c737cf8ac55e7c3a9292bf63d286b2
Pooja-D01 <poojasuresh8925@gmail.com>
2025-05-16 09:19:16 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (6.52%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -0,0 +1,25 @@
+class Solution {
+      List<Integer> num=new ArrayList<>();
+    public int fib(int n) {
+       
+        int a=0;
+        int b=1;
+        num.add(a);
+        num.add(b);
+        if(n<2){
+            return num.get(n);
+        }
+        return check(0,1,n);
+    }
+        int check(int a,int b,int n){
+            int next=a+b;
+            
+            num.add(next);
+            if(num.size()>n){
+                return num.get(n);
+            }
+            return check(b,next,n);
+        }
+       
+        
+    }
