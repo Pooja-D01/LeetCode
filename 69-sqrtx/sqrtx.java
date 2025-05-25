df32d6bb1fc68645f0f8119d770baaa360a6b8ef
Pooja-D01 <poojasuresh8925@gmail.com>
2025-05-25 21:30:52 +0530

    Time: 61 ms (5.15%) | Memory: 40.9 MB (54.34%) - LeetSync

---
+++ 69-sqrtx/sqrtx.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int mySqrt(int x) {
+        if (x == 0 || x == 1) return x;
+
+        for (int i = 1; i <= x / 2; i++) {
+            if (i * i == x) {
+                return i;
+            } 
+            else if ((long)i * i > x) {
+                return i - 1;
+            }
+        }
+        return x / 2; 
+    }
+}
