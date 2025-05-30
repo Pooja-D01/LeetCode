b9133362d57f40df6148f96267920d8e8ce91426
Pooja-D01 <poojasuresh8925@gmail.com>
2025-05-30 09:40:54 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (80.71%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int n = s.length();
+        int count = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            if (s.charAt(i) == ' ' && count > 0) {
+                return count;
+            } else if (s.charAt(i) != ' ') {
+                count++;
+            }
+        }
+        return count;
+    }
+}
