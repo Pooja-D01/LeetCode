c62c00c41dddf5c9e90337520c67ff07318ff112
Pooja-D01 <poojasuresh8925@gmail.com>
2025-07-23 15:42:14 +0530

    Time: 11 ms (1.36%) | Memory: 42.5 MB (9.42%) - LeetSync

--- 66-plus-one/plus-one.java
+++ 66-plus-one/plus-one.java
@@ -1,17 +1,16 @@
 import java.math.BigInteger;
-
 class Solution {
     public int[] plusOne(int[] digits) {
-        String val = "";
-        for (int d : digits) {
-            val = val + d;
+        String val="";
+        for(int i=0;i<digits.length;i++){
+            val=val+digits[i];
         }
-        BigInteger num = new BigInteger(val);
-        BigInteger plusOne = num.add(BigInteger.ONE);
-        String pluse = plusOne.toString();
-        int[] arr = new int[pluse.length()];
-        for (int i = 0; i < pluse.length(); i++) {
-            arr[i] = pluse.charAt(i) - '0';
+        BigInteger a=new BigInteger(val);
+        BigInteger sum=a.add(BigInteger.ONE);
+        String s=String .valueOf(sum);
+        int[] arr = new int[s.length()];
+        for(int i=0;i<s.length();i++){
+            arr[i]=s.charAt(i)-'0';
         }
         return arr;
     }
